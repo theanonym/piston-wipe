36d699c2def48712d25627b9b05f677792d3423e
theanonym <0theanonym@gmail.com>
2012-12-21 10:49:57 +0400

    Исправлены мелкие ошибки

--- lib/Piston/Engines/Nullchan.pm
+++ lib/Piston/Engines/Nullchan.pm
@@ -84,7 +84,7 @@ sub handle_captcha_response($)
    if(exists $response->{_headers}->{"content-type"} && $response->{_headers}->{"content-type"} =~ /image\/$cfmt/)
    {
       ($errcode, $errstr) = (0, "");
-      # write_file("nullchan_good_proxy.txt", { append => 1 }, "$$wipe{proxy}\n");
+      write_file("nullchan_good_proxy.txt", { append => 1 }, "$$wipe{proxy}\n");
    }
 
    # Фатальная ошибка (код 2)
@@ -162,7 +162,7 @@ sub handle_post_response($)
    if($errstr ~~ ["YOU ARE BANNED", "possible proxy"])
    {
       $errcode = 3;
-      write_file("bad_proxy.txt", { append => 1 }, "$$wipe{proxy}\n");
+      write_file("nullchan_bad_proxy.txt", { append => 1 }, "$$wipe{proxy}\n");
    }
 
    # Пост отправлен успешно (код 0)
